063f07c56108166c77ae9d31f82f6bca2633ae40
adarsh-naik-2004 <adarshnaik270@gmail.com>
2025-03-09 11:38:46 +0530

    env setup

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,3 @@
-function welcome(name: string) {
-    console.log('hi')
-    const user = {
-        name: 'wo',
-    }
-    const u1 = user.name
-    return name + u1
-}
-welcome('adarsh')
+import { Config } from './config'
+
+console.log(Config.PORT)
